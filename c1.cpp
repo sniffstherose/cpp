aa5e660d6ff00c2316a2e1ecc0f081f9829d4997
sniffstherose <3383876329@qq.com>
2023-05-28 16:04:18 +0800

    移动语义

--- c1.cpp
+++ c1.cpp
@@ -1,136 +1,109 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+#include <chrono>
 #include <iostream>
 #include <cstring>
 
 class MyString {
-public:
-	// 默认构造函数
-	MyString() : data(nullptr), length(0) {}
-
-	// 构造函数，接受C风格字符串作为参数
-	MyString(const char* str) {
-		length = std::strlen(str);
-		data = new char[length + 1];
-		std::strcpy(data, str);
-	}
-
-	// 拷贝构造函数
-	MyString(const MyString& other) {
-		length = other.length;
-		data = new char[length + 1];
-		std::strcpy(data, other.data);
-	}
-
-	// 移动构造函数
-	MyString(MyString&& other) noexcept {
-		length = other.length;
-		data = other.data;
-		other.length = 0;
-		other.data = nullptr;
-	}
-
-	// 析构函数
-	~MyString() {
-		delete[] data;
-	}
-
-	// 重载赋值操作符
-	MyString& operator=(const MyString& other) {
-		if (this != &other) {
-			delete[] data;
-			length = other.length;
-			data = new char[length + 1];
-			std::strcpy(data, other.data);
-		}
-		return *this;
-	}
-
-	// 重载+=操作符
-	MyString& operator+=(const MyString& other) {
-		char* temp = new char[length + other.length + 1];
-		std::strcpy(temp, data);
-		std::strcat(temp, other.data);
-		delete[] data;
-		data = temp;
-		length += other.length;
-		return *this;
-	}
-
-	// 重载+操作符
-	MyString operator+(const MyString& other) const {
-		MyString result(*this);
-		result += other;
-		return result;
-	}
-
-	// 重载<<操作符
-	friend std::ostream& operator<<(std::ostream& os, const MyString& str) {
-		os << str.data;
-		return os;
-	}
-
-	// 重载[]操作符
-	char& operator[](int index) {
-		return data[index];
-	}
-
 private:
-	char* data;
-	size_t length;
+    char* data;
+    size_t length;
+
+public:
+    // 默认构造函数
+    MyString() : data(nullptr), length(0) {}
+
+    // 带参构造函数
+    MyString(const char* str) {
+        length = std::strlen(str);
+        data = new char[length + 1];
+        std::strcpy(data, str);
+    }
+
+    // 拷贝构造函数
+    MyString(const MyString& other) {
+        length = other.length;
+        data = new char[length + 1];
+        std::strcpy(data, other.data);
+    }
+
+    // 移动构造函数
+    MyString(MyString&& other) noexcept {
+        length = other.length;
+        data = other.data;
+        other.length = 0;
+        other.data = nullptr;
+    }
+
+    // 析构函数
+    ~MyString() {
+        delete[] data;
+    }
+
+    // 重载赋值操作符=
+    MyString& operator=(const MyString& other) {
+        if (this != &other) {
+            delete[] data;
+            length = other.length;
+            data = new char[length + 1];
+            std::strcpy(data, other.data);
+        }
+        return *this;
+    }
+
+    // 重载+=操作符
+    MyString& operator+=(const MyString& other) {
+        size_t newLength = length + other.length;
+        char* newData = new char[newLength + 1];
+        std::strcpy(newData, data);
+        std::strcat(newData, other.data);
+        delete[] data;
+        data = newData;
+        length = newLength;
+        return *this;
+    }
+
+    // 重载+操作符
+    MyString operator+(const MyString& other) const {
+        MyString result(*this);
+        result += other;
+        return result;
+    }
+
+    // 获取字符串长度
+    size_t getLength() const {
+        return length;
+    }
+
+    // 获取字符串内容
+    const char* getData() const {
+        return data;
+    }
 };
 
 int main() {
-	// 创建一个MyString对象，并输出
-	MyString str1("Hello");
-	std::cout << "str1: " << str1 << std::endl;
+    const int N = 1000000;
 
-	// 使用拷贝构造函数创建一个新的MyString对象
-	MyString str2(str1);
-	std::cout << "str2: " << str2 << std::endl;
+    // 不使用移动构造函数的情况下创建对象
+    auto start = std::chrono::steady_clock::now();
+    MyString str1("Hello");
+    for (int i = 0; i < N; ++i) {
+        MyString temp(str1);
+    }
+    auto end = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "不使用移动语义 " << duration.count() << " milliseconds" << std::endl;
 
-	// 使用移动构造函数创建一个新的MyString对象
-	MyString str3(std::move(str1));
-	std::cout << "str3: " << str3 << std::endl;
+    // 使用移动构造函数的情况下创建对象
+    start = std::chrono::steady_clock::now();
+    MyString str2("Hello");
+    for (int i = 0; i < N; ++i) {
+        MyString temp(std::move(str2));
+    }
+    end = std::chrono::steady_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "使用移动语义： " << duration.count() << " milliseconds" << std::endl;
 
-	// 使用赋值操作符进行赋值，并输出结果
-	str2 = str3;
-	std::cout << "str2 after assignment: " << str2 << std::endl;
-
-	// 使用+=操作符进行字符串连接，并输出结果
-	str2 += " World";
-	std::cout << "str2 after +=: " << str2 << std::endl;
-
-	// 使用+操作符进行字符串连接，并输出结果
-	MyString str4 = str2 + "!";
-	std::cout << "str4: " << str4 << std::endl;
-
-	// 修改特定索引位置的字符，并输出结果
-	str4[0] = 'G';
-	std::cout << "str4 after modification: " << str4 << std::endl;
-
-	//验证移动语义的效率提升
-		// 创建两个MyString对象
-	MyString str1("Hello");
-	MyString str2("World");
-
-	// 测试赋值操作符（使用深拷贝）
-	MyString str3 = str1;
-	std::cout << "str3: " << str3 << std::endl;
-
-	// 测试移动构造函数
-	MyString str4 = std::move(str2);
-	std::cout << "str4: " << str4 << std::endl;
-
-	// 测试+=操作符
-	str3 += str4;
-	std::cout << "str3 after +=: " << str3 << std::endl;
-
-	// 测试+操作符
-	MyString str5 = str1 + str4;
-	std::cout << "str5: " << str5 << std::endl;
-
-
-	return 0;
+    return 0;
 }
 
